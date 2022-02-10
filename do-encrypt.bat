b784d8ac5108bbe41214a8c25b0ad47a36424be7
deal <halx99@live.com>
2022-02-10 22:30:39 +0800

    Add directly copy types

--- do-encrypt.bat
+++ do-encrypt.bat
@@ -1,5 +1,5 @@
 @echo off
-"%XS_INSTDIR%\x-studio" -c -enc -cfg=.\encrypt-cfg.xml -i=.\assets-dev -o=.\assets
+"%XS_INSTDIR%\x-studio" -c -enc -cfg=.\encrypt-cfg.xml -i=.\assets-dev -o=.\assets -cp=.ttf;.mp3;.ogg;.mp4
 
 echo "Exiting ..."
 ping /n 2 127.0.0.1 >nul
